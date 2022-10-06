4b1d2804e0c0c7958ffcd852263e747e965910a2
NiNaa32 <112417273+NiNaa32@users.noreply.github.com>
2022-10-06 11:09:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("This program has a loop");
+Console.WriteLine("The loop calculates the sum of the numbers 0 to 4");
+int sum = 0;
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(" Now the value of i is printed ");
+    Console.WriteLine($"i = {i}");
+    // Increment the sum variable by the value of I
+    sum += i;
+}
+Console.WriteLine($" The sum of the numbers is {sum}");
